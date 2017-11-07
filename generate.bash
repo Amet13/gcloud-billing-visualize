b0429f617f97b5e63e79de5a015fd071ccbc9167
Amet13 <admin@amet13.name>
2017-11-07 14:00:29 +0300

    Add last month report

--- generate.bash
+++ generate.bash
@@ -8,6 +8,9 @@ PREFIX="report"
 BUCKET_PATH="./csv"
 DATE=$(date +"%Y-%m-%d" --date="2 days ago")
 #DATE="2017-11-03" # I'm using it for demo
+LAST_MONTH=$(date +'%Y-%m' -d 'last month')
+ENABLE_MONTH_REPORT=1
+
 FILENAME="${BUCKET_PATH}/${PREFIX}-${DATE}.csv"
 REPORT="csv/report.csv"
 INDEX_HTML="index.html"
@@ -26,8 +29,8 @@ SECTION=""
 > "${REPORT}"
 # Generate report.csv
 {
-echo "${COLUMNS}" | sed -r "s/Measurement1 //g" | sed -r "s/\"//g"
-awk -f "${AWKFILE}" -F "," -v cols="${COLUMNS}" "${FILENAME}"
+    echo "${COLUMNS}" | sed -r "s/Measurement1 //g" | sed -r "s/\"//g"
+    awk -f "${AWKFILE}" -F "," -v cols="${COLUMNS}" "${FILENAME}"
 } >> "${REPORT}"
 
 # Remove empty column
@@ -43,49 +46,65 @@ cat "${HEADER}" > "${INDEX_HTML}"
 sed -i -r "s/YYYY.MM.DD/$DATE/g" "${INDEX_HTML}"
 sed -i -r -e "s/XXX/$CE_COST/g" -e "s/YYY/$CS_COST/g" -e "s/ZZZ/$OTHER_COST/g" "${INDEX_HTML}"
 
-> "${TABLE}"
+# Generate report for last month
+LAST_MONTH_COST=0
+if [[ ${ENABLE_MONTH_REPORT} == "1" ]]; then
+    while read DAY; do
+        LAST_DAY_COST=$(awk -f "${AWKFILE}" -F "," -v cols=Cost "${DAY}" | sed -r 's/,//g' | paste -sd+ | bc)
+        LAST_MONTH_COST=$(echo "${LAST_MONTH_COST}" + "${LAST_DAY_COST}" | bc)
+    done < <(ls -1 "${BUCKET_PATH}"/"${PREFIX}"-"${LAST_MONTH}"-*.csv)
+    > "${TABLE}"
+    echo -e "<h1>Last month usage (${LAST_MONTH}): ${CURRENCY}${LAST_MONTH_COST}</h1>\n<hr>" >"${TABLE}"
+fi
+
+# Add chart
+{
+    echo -e "<h1>Chart for ${DATE}:</h1>"
+    echo -e "<div id=\"chartdiv\"></div>\n<hr>"
+} >> "${TABLE}"
+
 # All services
 {
-echo -e "<h1>All services:</h1>\n${TABLE_CLASS}"
-head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
-tail -n +2 "${REPORT}" | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
-echo -e "${TABLE_END}"
-echo -e "<h2>Total cost: ${CURRENCY}${TOTAL_COST}</h2>"
+    echo -e "<h1>All services:</h1>\n${TABLE_CLASS}"
+    head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
+    tail -n +2 "${REPORT}" | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
+    echo -e "${TABLE_END}"
+    echo -e "<h2>Total cost: ${CURRENCY}${TOTAL_COST}</h2>"
 } >> "${TABLE}"
 
 # Compute engine
-{
 SECTION="compute-engine"
-echo -e "${HR}<h1>Compute engine:</h1>\n${TABLE_CLASS}"
-head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
-grep "${SECTION}" "${REPORT}" | tail -n +2 | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
-echo -e "${TABLE_END}"
-echo -e "<h2>Total cost: ${CURRENCY}${CE_COST}</h2>"
+{
+    echo -e "${HR}<h1>Compute engine:</h1>\n${TABLE_CLASS}"
+    head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
+    grep "${SECTION}" "${REPORT}" | tail -n +2 | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
+    echo -e "${TABLE_END}"
+    echo -e "<h2>Total cost: ${CURRENCY}${CE_COST}</h2>"
 } >> "${TABLE}"
 
 # Cloud storage
 SECTION="cloud-storage"
 {
-echo -e "${HR}<h1>Cloud storage:</h1>\n${TABLE_CLASS}"
-head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
-grep "${SECTION}" "${REPORT}" | tail -n +2 | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
-echo -e "${TABLE_END}"
-echo -e "<h2>Total cost: ${CURRENCY}${CS_COST}</h2>"
+    echo -e "${HR}<h1>Cloud storage:</h1>\n${TABLE_CLASS}"
+    head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
+    grep "${SECTION}" "${REPORT}" | tail -n +2 | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
+    echo -e "${TABLE_END}"
+    echo -e "<h2>Total cost: ${CURRENCY}${CS_COST}</h2>"
 } >> "${TABLE}"
 
 # Other
 {
-echo -e "${HR}<h1>Other services:</h1>\n${TABLE_CLASS}"
-head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
-egrep -v "cloud-storage|compute-engine" "${REPORT}" | tail -n +2 | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
-echo -e "${TABLE_END}"
-echo -e "<h2>Total cost: ${CURRENCY}${OTHER_COST}</h2>"
+    echo -e "${HR}<h1>Other services:</h1>\n${TABLE_CLASS}"
+    head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
+    egrep -v "cloud-storage|compute-engine" "${REPORT}" | tail -n +2 | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
+    echo -e "${TABLE_END}"
+    echo -e "<h2>Total cost: ${CURRENCY}${OTHER_COST}</h2>"
 } >> "${TABLE}"
 
 {
 cat "${TABLE}"
-echo -e "${HR}<p style=\"text-align:center\">\n<a target=\"_blank\" href=\"https://github.com/Amet13/gcloud-billing-visualize\" title=\"Hosted on GitHub\"><img src=\"images/github_80x15.png\" width=\"80\" height=\"15\" alt=\"Source on GitHub\"/></a>\n</p>"
-echo -e "</body>\n</html>"
+    echo -e "${HR}<p style=\"text-align:center\">\n<a target=\"_blank\" href=\"https://github.com/Amet13/gcloud-billing-visualize\" title=\"Hosted on GitHub\"><img src=\"images/github_80x15.png\" width=\"80\" height=\"15\" alt=\"Source on GitHub\"/></a>\n</p>"
+    echo -e "</body>\n</html>"
 } >> "${INDEX_HTML}"
 
 echo "Successfully generated new report"
