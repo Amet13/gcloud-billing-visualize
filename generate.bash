260018a180c81277a615e74b2c44efdd510bded2
Amet13 <admin@amet13.name>
2017-11-14 17:06:29 +0300

    Code refactoring, using functions

--- generate.bash
+++ generate.bash
@@ -7,7 +7,6 @@ bc --version > /dev/null
 # Edit this settings
 PREFIX="report"
 BUCKET_PATH="./csv"
-ENABLE_MONTH_REPORT=1
 # End settings edit
 
 DATE=$(date +"%Y-%m-%d" --date="yesterday")
@@ -32,7 +31,6 @@ CURRENCY="$"
 ROW="<div class=\"row\">"
 COLUMN="<div class=\"column\">"
 DIV="</div>"
-SHOW_HIDE="<a id=\"${SECTION}\" href=\"javascript:toggle2('${SECTION}','${SECTION}');\">"
 
 if [[ ! -f ${FILENAME} ]]; then
     echo "File ${FILENAME} does not exists"
@@ -41,7 +39,22 @@ fi
 
 # Function for showing and hiding info for sections
 show_hide () {
+    SECTION=${1}
     SHOW_HIDE="<a id=\"my${SECTION}\" href=\"javascript:toggle2('${SECTION}','my${SECTION}');\">show/hide</a>"
+}
+
+# Function for generating tables
+table_gen () {
+    echo -e "${TABLE_CLASS}"
+    head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
+    if [[ ${SECTION} == "all" ]]; then
+        tail -n +2 "${REPORT}" | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
+    elif [[ ${SECTION} == "compute-engine" ]] || [[ ${SECTION} == "cloud-storage" ]]; then
+        grep "${SECTION}" "${REPORT}" | tail -n +2 | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
+    else
+        egrep -v "cloud-storage|compute-engine" "${REPORT}" | tail -n +2 | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
+    fi
+    echo -e "${TABLE_END}"
 }
 
 > "${REPORT}"
@@ -59,34 +72,32 @@ CE_COST=$(egrep "${compute-engine}|Cost" "${REPORT}" | awk -f "${AWKFILE}" -F ",
 CS_COST=$(egrep "${cloud-storage}|Cost" "${REPORT}" | awk -f "${AWKFILE}" -F "," -v cols=Cost  | sed -r 's/,//g' | paste -sd+ | bc)
 OTHER_COST=$(egrep -v "cloud-storage|compute-engine" "${REPORT}" | awk -f "${AWKFILE}" -F "," -v cols=Cost  | sed -r 's/,//g' | paste -sd+ | bc)
 NEXT_MONTH_COST=$(echo "${TOTAL_COST} * 30" | bc)
+LAST_MONTH_COST=0
+CURRENT_MONTH_COST=0
 
 # Generate html table
 cat "${HEADER}" > "${INDEX_HTML}"
 sed -i -r "s/YYYY.MM.DD/$DATE/g" "${INDEX_HTML}"
 sed -i -r -e "s/XXX/$CE_COST/g" -e "s/YYY/$CS_COST/g" -e "s/ZZZ/$OTHER_COST/g" "${INDEX_HTML}"
 
-# Generate report for last and current month
-LAST_MONTH_COST=0
-CURRENT_MONTH_COST=0
-if [[ ${ENABLE_MONTH_REPORT} == "1" ]]; then
-    while read DAY; do
-        LAST_DAY_COST=$(awk -f "${AWKFILE}" -F "," -v cols=Cost "${DAY}" | sed -r 's/,//g' | paste -sd+ | bc)
-        LAST_MONTH_COST=$(echo "${LAST_MONTH_COST}" + "${LAST_DAY_COST}" | bc)
-    done < <(ls -1 "${BUCKET_PATH}"/"${PREFIX}"-"${LAST_MONTH}"-*.csv)
-    while read DAY; do
-        CURRENT_DAY_COST=$(awk -f "${AWKFILE}" -F "," -v cols=Cost "${DAY}" | sed -r 's/,//g' | paste -sd+ | bc)
-        CURRENT_MONTH_COST=$(echo "${CURRENT_MONTH_COST}" + "${CURRENT_DAY_COST}" | bc)
-    done < <(ls -1 "${BUCKET_PATH}"/"${PREFIX}"-"${CURRENT_MONTH}"-*.csv)
-    > "${TABLE}"
-    {
-        echo -e "\n${ROW}"
-        echo -e "  ${COLUMN}<h1>Last month cost (${LAST_MONTH}): ${CURRENCY}${LAST_MONTH_COST}</h1>${DIV}"
-        echo -e "  ${COLUMN}<h1>Current month cost (${CURRENT_MONTH}): ${CURRENCY}${CURRENT_MONTH_COST}</h1>${DIV}"
-        echo -e "  ${COLUMN}<h1>Daily cost (${DATE}): ${CURRENCY}${TOTAL_COST}</h1>${DIV}"
-        echo -e "  ${COLUMN}<h1>Next month cost prediction (${NEXT_MONTH}): ${CURRENCY}${NEXT_MONTH_COST}</h1>${DIV}"
-        echo -e "${DIV}\n\n<hr>\n"
-    } >> "${TABLE}"
-fi
+# Generate report for last, current and next month
+while read DAY; do
+    LAST_DAY_COST=$(awk -f "${AWKFILE}" -F "," -v cols=Cost "${DAY}" | sed -r 's/,//g' | paste -sd+ | bc)
+    LAST_MONTH_COST=$(echo "${LAST_MONTH_COST}" + "${LAST_DAY_COST}" | bc)
+done < <(ls -1 "${BUCKET_PATH}"/"${PREFIX}"-"${LAST_MONTH}"-*.csv)
+while read DAY; do
+    CURRENT_DAY_COST=$(awk -f "${AWKFILE}" -F "," -v cols=Cost "${DAY}" | sed -r 's/,//g' | paste -sd+ | bc)
+    CURRENT_MONTH_COST=$(echo "${CURRENT_MONTH_COST}" + "${CURRENT_DAY_COST}" | bc)
+done < <(ls -1 "${BUCKET_PATH}"/"${PREFIX}"-"${CURRENT_MONTH}"-*.csv)
+> "${TABLE}"
+{
+    echo -e "\n${ROW}"
+    echo -e "  ${COLUMN}<h1>Last month cost (${LAST_MONTH}): ${CURRENCY}${LAST_MONTH_COST}</h1>${DIV}"
+    echo -e "  ${COLUMN}<h1>Current month cost (${CURRENT_MONTH}): ${CURRENCY}${CURRENT_MONTH_COST}</h1>${DIV}"
+    echo -e "  ${COLUMN}<h1>Daily cost (${DATE}): ${CURRENCY}${TOTAL_COST}</h1>${DIV}"
+    echo -e "  ${COLUMN}<h1>Next month cost prediction (${NEXT_MONTH}): ${CURRENCY}${NEXT_MONTH_COST}</h1>${DIV}"
+    echo -e "${DIV}\n\n<hr>\n"
+} >> "${TABLE}"
 
 # Add chart
 {
@@ -95,62 +106,43 @@ fi
 } >> "${TABLE}"
 
 # All services
-SECTION="ALL"
-show_hide
+show_hide "all"
 {
     echo -e "${HR}<h1>All services (${CURRENCY}${TOTAL_COST}): ${SHOW_HIDE}</h1>\n<div id=\"${SECTION}\">"
-    echo -e "${TABLE_CLASS}"
-    head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
-    tail -n +2 "${REPORT}" | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
-    echo -e "${TABLE_END}"
+    table_gen
     echo -e "<h2>Total cost: ${CURRENCY}${TOTAL_COST}</h2>"
     echo -e "${DIV}"
 } >> "${TABLE}"
 
 # Compute engine
-SECTION="compute-engine"
-show_hide
+show_hide "compute-engine"
 {
-    echo -e "${HR}<h1>Compute engine (${CURRENCY}${CE_COST}):"
-    echo -e "${SHOW_HIDE}</h1>\n<div id=\"${SECTION}\">"
-    echo -e "${TABLE_CLASS}"
-    head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
-    grep "${SECTION}" "${REPORT}" | tail -n +2 | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
-    echo -e "${TABLE_END}"
+    echo -e "${HR}<h1>Compute engine (${CURRENCY}${CE_COST}): ${SHOW_HIDE}</h1>\n<div id=\"${SECTION}\">"
+    table_gen
     echo -e "<h2>Total cost: ${CURRENCY}${CE_COST}</h2>"
     echo -e "${DIV}"
 } >> "${TABLE}"
 
 # Cloud storage
-SECTION="cloud-storage"
-show_hide
+show_hide "cloud-storage"
 {
-    echo -e "${HR}<h1>Cloud storage (${CURRENCY}${CS_COST}):"
-    echo -e "${SHOW_HIDE}</h1>\n<div id=\"${SECTION}\">"
-    echo -e "${TABLE_CLASS}"
-    head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
-    grep "${SECTION}" "${REPORT}" | tail -n +2 | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
-    echo -e "${TABLE_END}"
+    echo -e "${HR}<h1>Cloud storage (${CURRENCY}${CS_COST}): ${SHOW_HIDE}</h1>\n<div id=\"${SECTION}\">"
+    table_gen
     echo -e "<h2>Total cost: ${CURRENCY}${CS_COST}</h2>"
     echo -e "${DIV}"
 } >> "${TABLE}"
 
-# Other
-SECTION="other"
-show_hide
+# Other services
+show_hide "other"
 {
-    echo -e "${HR}<h1>Other services (${CURRENCY}${OTHER_COST}):"
-    echo -e "${SHOW_HIDE}</h1>\n<div id=\"${SECTION}\">"
-    echo -e "${TABLE_CLASS}"
-    head -1 "${REPORT}" | sed -e "s/^/<tr>\n  <th>/" -e "s/,/<\/th>\n  <th>/g" -e "s/$/<\/th>\n<\/tr>/"
-    egrep -v "cloud-storage|compute-engine" "${REPORT}" | tail -n +2 | sed -e "s/^/<tr>\n  <td>/" -e "s/,/<\/td>\n  <td>/g" -e "s/$/<\/td>\n<\/tr>/"
-    echo -e "${TABLE_END}"
+    echo -e "${HR}<h1>Other services (${CURRENCY}${OTHER_COST}): ${SHOW_HIDE}</h1>\n<div id=\"${SECTION}\">"
+    table_gen
     echo -e "<h2>Total cost: ${CURRENCY}${OTHER_COST}</h2>"
     echo -e "${DIV}"
 } >> "${TABLE}"
 
 {
-cat "${TABLE}"
+    cat "${TABLE}"
     echo -e "${HR}<p style=\"text-align:center\">\n<a target=\"_blank\" href=\"https://github.com/Amet13/gcloud-billing-visualize\" title=\"Hosted on GitHub\"><img src=\"images/github_80x15.png\" width=\"80\" height=\"15\" alt=\"Source on GitHub\"/></a>\n</p>"
     echo -e "</body>\n</html>"
 } >> "${INDEX_HTML}"
